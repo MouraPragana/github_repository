4ad2cd4b1a1a6ab0a54415ecd0064cf91b6c3129
Mateus Moura <mateuspragana@gmail.com>
2023-07-13 09:50:16 -0300

    feat: finalizando aula - Estilizando pagina Main

--- src/page/Main/index.tsx
+++ src/page/Main/index.tsx
@@ -1,5 +1,25 @@
-import { Title } from "./styles";
+import { Container, Form, SubmitButton } from "./styles";
+import { FaGithub, FaPlus } from "react-icons/fa";
 
 export function Main() {
-  return <Title>Main</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithub size={25} />
+        Meus Repositorios
+      </h1>
+
+      <Form
+        onSubmit={() => {
+          console.log();
+        }}
+      >
+        <input type="text" placeholder="Adicionar Repositórios" />
+
+        <SubmitButton>
+          <FaPlus size={14} color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
