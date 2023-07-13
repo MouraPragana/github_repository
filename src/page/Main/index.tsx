fcfcb81751da2bd112b40796bb596982ae5004aa
Mateus Moura <mateuspragana@gmail.com>
2023-07-13 13:06:36 -0300

    feat: finalizando aula - Buscando repos

--- src/page/Main/index.tsx
+++ src/page/Main/index.tsx
@@ -1,7 +1,35 @@
-import { Container, Form, SubmitButton } from "./styles";
+import { ChangeEvent, FormEvent, useState, useCallback } from "react";
 import { FaGithub, FaPlus } from "react-icons/fa";
+import { Container, Form, SubmitButton } from "./styles";
+import { api } from "../../services/api";
+
+interface IResponse {
+  full_name: string;
+}
 
 export function Main() {
+  const [newRepo, setNewRepo] = useState<string>("");
+  const [repositorios, setRepositorios] = useState<string[]>([]);
+
+  function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+    setNewRepo(e.target.value);
+  }
+
+  const handleSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      async function submit() {
+        const { data } = await api.get<IResponse>(`repos/${newRepo}`);
+        setRepositorios((oldState) => [...oldState, data.full_name]);
+        setNewRepo("");
+      }
+
+      void submit();
+    },
+    [newRepo, setNewRepo]
+  );
+
   return (
     <Container>
       <h1>
@@ -9,17 +37,21 @@ export function Main() {
         Meus Repositorios
       </h1>
 
-      <Form
-        onSubmit={() => {
-          console.log();
-        }}
-      >
-        <input type="text" placeholder="Adicionar Repositórios" />
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicionar Repositórios"
+          value={newRepo}
+          onChange={handleInputChange}
+        />
 
-        <SubmitButton>
+        <SubmitButton type="submit">
           <FaPlus size={14} color="#FFF" />
         </SubmitButton>
       </Form>
+      {repositorios.map((repo) => (
+        <div>{repo}</div>
+      ))}
     </Container>
   );
 }
